ac4022a2160b82ecac98cbee943f2e9fa6fece56
Sam Barnett <samuel.barnett2@mail.dcu.ie>
2024-02-11 21:42:21 +0000

    Changed AI state_1 probability actions. Will no longer fold on large min bets

--- code/HeadsUpPoker/ai_actions.py
+++ code/HeadsUpPoker/ai_actions.py
@@ -5,13 +5,21 @@ class Bot:
 
     def call(self, bet):
         if self.chips > bet:
+            self.chips -= bet
             return ["call", bet]
         else:
-            return ["all_in", self.chips]
+            raise_amount = self.chips
+            self.chips = 0
+            return ["all_in", raise_amount]
         
     def raise_bet(self, raise_amount): # Checks will be handled in use to ensure raise isnt greater than AI chips.
-        self.chips -= raise_amount
-        return ["raise", raise_amount]
+        if self.chips < raise_amount:
+            raise_amount = self.chips
+            self.chips = 0
+            return ["raise", raise_amount]
+        else:
+            self.chips -= raise_amount
+            return ["raise", raise_amount]
 
 
     def fold(self): #AI will fold if it believes it chance of winning is slim
--- code/HeadsUpPoker/ai_play_state_1.py
+++ code/HeadsUpPoker/ai_play_state_1.py
@@ -6,14 +6,14 @@ if not, the computer will check or do a small raise.
 
 Note for possible changes: Deal with raises less than 1/8 of ai's chips, 1/4 etc...
 
-Possible issues: not much variation between way AI bets when player does small raise to medium raise.
+Possible issues: not much variation between way AI bets when player does small raise to when player does medium raise.
 --------------------------------------------------------------------------------
 '''
 import random
 import numpy as np
 from chen_formula import chen_formula
 
-def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
+def play_state_1(ai_bot, hand, small_blind_amount, raise_state):
 
     chen_score = chen_formula(hand)
 
@@ -21,14 +21,14 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
     if raise_state[0] == False:
 
         x = random.randint(10, 21)
-        if big_blind_amount < 5000:
+        if small_blind_amount < 5000:
             shape_a, shape_b, shape_c = 0.25, 0.15, 0.02
         else:
             shape_a, shape_b, shape_c = 0.35, 0.25, 0.05 # AI will do higher bets as the blinds increase...
 
         if x <= chen_score: # random value was less than chen score. (Only chen score's 10 or better will do larger raises)
 
-            min_bet = big_blind_amount * 2
+            min_bet = small_blind_amount * 2
             max_bet = ai_bot.chips
             raise_risk_decider = random.randint(1, 21)
 
@@ -65,7 +65,7 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
         
             else: #There is a 1 in 3 chance the ai will do a small raise to make it harder to predict.
 
-                min_bet = big_blind_amount
+                min_bet = small_blind_amount
                 max_bet = ai_bot.chips // 2
                 # Adjusting for lower risk
                 rand_exp = np.random.exponential(scale=shape_c)
@@ -85,7 +85,7 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
             x = random.randint(1, 51) 
             if x <= chen_score: # Will raise 20% of the time with a chen score of 10 and 40% of the time with chen score of 20
                 shape = 0.006 * chen_score
-                min_bet = raise_state[1]
+                min_bet = raise_state[1] + 100
                 max_bet = ai_bot.chips
                 # Adjusting for lower risk
                 rand_exp = np.random.exponential(scale=shape)
@@ -105,28 +105,37 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
 
             if chen_score > 5:
             
-                if raise_state[1] > ai_bot.chips // 2 and big_blind_amount < ai_bot.chips // 16: # Raise by player is greater than half of ai's chips and small big blind
+                if raise_state[1] > ai_bot.chips // 2 and small_blind_amount < ai_bot.chips // 6: # Raise by player is greater than half of ai's chips and small big blind
                     x = random.randint(5, 16)
                     if x <= chen_score -3: # AI will play 20% of time with 9 and 10% of time with 8, otherwise deemed not worth the risk
                         return ai_bot.call(raise_state[1])
                     else:
-                        return ai_bot.fold() # Always fold with chen score of 7 or 6
-
-                elif raise_state[1] > ai_bot.chips // 2 and big_blind_amount >= ai_bot.chips // 16: # Raise by player is greater than half of ai's chips and large big blind
-                    x = random.randint(1, 11)
-                    if x <= chen_score: 
                         return ai_bot.call(raise_state[1])
+
+                elif raise_state[1] > ai_bot.chips // 2 and small_blind_amount >= ai_bot.chips // 6: # Raise by player is greater than half of ai's chips and large big blind
+                    x = random.randint(1, 11)
+                    if x <= chen_score : 
+                        shape = 0.02 # Skew more towards very small raises
+                        min_bet = raise_state[1] + 100
+                        max_bet = ai_bot.chips // 2
+                        rand_exp = np.random.exponential(scale=shape)
+                        scaled_value = min_bet + (max_bet - min_bet) * (1 - np.exp(-rand_exp))
+                        # Ensure that the scaled value is within the desired range
+                        raise_amount = min(max_bet, max(min_bet, int(scaled_value)))
+                        # Ensure there is a difference of 100 between each choice
+                        raise_amount = round(raise_amount / 100) * 100
+                        return ai_bot.raise_bet(raise_amount)
                     else:
-                        return ai_bot.fold()  # 40% chance of folding a 6... 10% of folding 9.
+                        return ai_bot.call(raise_state[1]) # 40% chance of folding a 6... 10% of folding 9.
                 
 
-                elif raise_state[1] <= ai_bot.chips // 2 and big_blind_amount < ai_bot.chips // 16: # Raise by player is less than half of ai's chips. and small big blind
+                elif raise_state[1] <= ai_bot.chips // 2 and small_blind_amount < ai_bot.chips // 6: # Raise by player is less than half of ai's chips. and small big blind
                     x = random.randint(1, 21)
                     if x <= chen_score + 12: # Will always call/reraise with a chen score of 8 or 9
 
                         if random.randint(1, 4) == 1: # 33.3% chance of re-raising.
                             shape = 0.03
-                            min_bet = raise_state[1]
+                            min_bet = raise_state[1] + 100
                             max_bet = ai_bot.chips // 2
                             # Adjusting for smaller bets more often
                             rand_exp = np.random.exponential(scale=shape)
@@ -143,13 +152,13 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
                         return ai_bot.fold() # fold 10% with a 6 and 5% with a 7.
                 
 
-                elif raise_state[1] <= ai_bot.chips // 2 and big_blind_amount >= ai_bot.chips // 16: # Raise by player is less than half of ai's chips and large big blind
-                    x = random.randint(1, 11)
-                    if x <= chen_score + 3: #Play with a 7, 8, 9 chen score and 90% of the time with a 6.
+                elif raise_state[1] <= ai_bot.chips // 2 and small_blind_amount >= ai_bot.chips // 6: # Raise by player is less than half of ai's chips and large big blind
+                    x = random.randint(1, 51)
+                    if x <= chen_score + 43: #Play with a 7, 8, 9 chen score and 90% of the time with a 6.
 
                         if random.randint(1, 4) == 1: # 33% chance of re-raising.
                             shape = 0.09
-                            min_bet = raise_state[1]
+                            min_bet = raise_state[1] + 100
                             max_bet = ai_bot.chips // 2
                             # Adjusting for smaller bets more often
                             rand_exp = np.random.exponential(scale=shape)
@@ -163,11 +172,12 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
                             return ai_bot.call(raise_state[1]) # Call 67% of the time
                         
                     else:
-                        return ai_bot.fold() # Fold 10% of the time if chen is 6
+                        return ai_bot.fold() # Fold 2% of the time if chen is 6
                     
                 else: 
                     return "ERROR when computing state 'Chen Score less than 10, greater than 5'"    
                 
+
 
 
             else: # Chen Score is less than or equal to 5 (WEAK POCKET)
@@ -175,37 +185,55 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
                     chen_score = 1
 
 
-                if raise_state[1] > ai_bot.chips // 2 and big_blind_amount < ai_bot.chips // 16: # Raise by player is greater than half of ai's chips and small big blind
+                if raise_state[1] > ai_bot.chips // 2 and small_blind_amount < ai_bot.chips // 6: # Raise by player is greater than half of ai's chips and small big blind
                     #WEAK HAND, BIG RAISE, small min bet
                     return ai_bot.fold() # Always fold weak hand with small min bet
 
 
-                elif raise_state[1] > ai_bot.chips // 2 and big_blind_amount >= ai_bot.chips // 16: # Raise by player is greater than half of ai's chips and large big blind
+                elif raise_state[1] > ai_bot.chips // 2 and small_blind_amount >= ai_bot.chips // 6: # Raise by player is greater than half of ai's chips and large big blind
                     #WEAK HAND, BIG RAISE, Large min bet
-                    if big_blind_amount > ai_bot.chips // 8: # Very Large min bet
+                    if small_blind_amount > ai_bot.chips // 3: # Very Large min bet
 
-                        x = random.randint(2, 10) # 50% chance of playing with 5 score, 12.5% chance of playing with 2 score 
+                        x = random.randint(2, 10) 
                         if x <= chen_score + 2:
-                            return ai_bot.call(raise_state[1])
+                            shape = 0.02 # Skew more towards very small raises
+                            min_bet = raise_state[1] + 100
+                            max_bet = ai_bot.chips // 2
+                            rand_exp = np.random.exponential(scale=shape)
+                            scaled_value = min_bet + (max_bet - min_bet) * (1 - np.exp(-rand_exp))
+                            # Ensure that the scaled value is within the desired range
+                            raise_amount = min(max_bet, max(min_bet, int(scaled_value)))
+                            # Ensure there is a difference of 100 between each choice
+                            raise_amount = round(raise_amount / 100) * 100
+                            return ai_bot.raise_bet(raise_amount)
                         else:
-                            return ai_bot.fold() # Always fold a 1 or lower chen score. 
+                            return ai_bot.call(raise_state[1])
 
-                    else: # Slighly more ability to fold these hands
-                        x = random.randint(3,15) # 8.33% of playing with 3 score, 16.67% chance of playing with 4 score, 25% with 5 score 
+                    else: 
+                        x = random.randint(3,15)
                         if x <= chen_score:
-                            return ai_bot.call(raise_state[1])
+                            shape = 0.02 # Skew more towards very small raises
+                            min_bet = raise_state[1] + 100
+                            max_bet = ai_bot.chips // 2
+                            rand_exp = np.random.exponential(scale=shape)
+                            scaled_value = min_bet + (max_bet - min_bet) * (1 - np.exp(-rand_exp))
+                            # Ensure that the scaled value is within the desired range
+                            raise_amount = min(max_bet, max(min_bet, int(scaled_value)))
+                            # Ensure there is a difference of 100 between each choice
+                            raise_amount = round(raise_amount / 100) * 100
+                            return ai_bot.raise_bet(raise_amount)
                         else:
-                            return ai_bot.fold() # Always fold a 2 or lower chen score. 
+                            return ai_bot.call(raise_state[1])
                 
 
-                elif raise_state[1] <= ai_bot.chips // 2 and big_blind_amount < ai_bot.chips // 16: # Raise by player is less than half of ai's chips. and small big blind
+                elif raise_state[1] <= ai_bot.chips // 2 and small_blind_amount < ai_bot.chips // 6: # Raise by player is less than half of ai's chips. and small big blind
                     #WEAK HAND, small RAISE, small min bet # Almost never fold
                     x = random.randint(1, 21)
                     if x <= chen_score + 15: # Will always call with 5 and 80% of the time with 1.
 
                         if random.randint(1, 11) == 1: # 10% chance of re-raising.
                             shape = 0.01 # Skew more towards very small raises
-                            min_bet = raise_state[1]
+                            min_bet = raise_state[1] + 100
                             max_bet = ai_bot.chips // 2
                             rand_exp = np.random.exponential(scale=shape)
                             scaled_value = min_bet + (max_bet - min_bet) * (1 - np.exp(-rand_exp))
@@ -221,14 +249,14 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
                         return ai_bot.fold() # fold 20% with a 1 score etc..
                 
 
-                elif raise_state[1] <= ai_bot.chips // 2 and big_blind_amount >= ai_bot.chips // 16: # Raise by player is less than half of ai's chips and large big blind
+                elif raise_state[1] <= ai_bot.chips // 2 and small_blind_amount >= ai_bot.chips // 6: # Raise by player is less than half of ai's chips and large big blind
                     #WEAK HAND, Small RAISE, Large min bet
-                    x = random.randint(1, 11)
-                    if x <= chen_score + 7: #Play 80% of the time with 1 and alwyas with 3, 4 and 5
+                    x = random.randint(1, 21)
+                    if x <= chen_score + 18: #Play 95% of the time with 1 and alwyas with 3, 4 and 5
 
                         if random.randint(1, 6) == 1: # 20% chance of re-raising.
                             shape = 0.09
-                            min_bet = raise_state[1]
+                            min_bet = raise_state[1] + 100
                             max_bet = ai_bot.chips // 2
                             # Adjusting for smaller bets more often
                             rand_exp = np.random.exponential(scale=shape)
@@ -242,7 +270,7 @@ def play_state_1(ai_bot, hand, big_blind_amount, raise_state):
                             return ai_bot.call(raise_state[1]) # Call 80% of the time
                         
                     else:
-                        return ai_bot.fold() # Fold 20% of time score is 1 etc..
+                        return ai_bot.fold() # Fold 5% of time score is 1 etc..
                     
 
                 else: 
