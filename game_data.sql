4922cedaa4ea3662a7e9a6f8672d4661c42e2d66
conor tilley <conortilley12@gmail.com>
2024-02-12 14:24:10 +0000

    Removed sql files

--- game_data.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE games (
-    game_id INT AUTO_INCREMENT PRIMARY KEY,
-    table_name VARCHAR(255),
-    community_cards VARCHAR(255),
-    pot_size INT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
