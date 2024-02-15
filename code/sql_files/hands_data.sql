5b001100e28ad257b8e760c53f8d4ee4e0be0990
conor tilley <conortilley12@gmail.com>
2024-02-15 14:56:40 +0000

    added data to hands_data.sql file

--- code/sql_files/hands_data.sql
+++ code/sql_files/hands_data.sql
@@ -1,12 +1,14 @@
 CREATE TABLE hands (
     hand_id INT AUTO_INCREMENT PRIMARY KEY,
     table_name VARCHAR(50), -- Pluribus
-    blind_small INT, -- seat number for who was the small blind
-    blind_big INT -- seat number for who was the big blind
-    -- hand_number INT,
-    -- game_type VARCHAR(50),
-    -- max_players INT, -- Is 6 but want to have it only for two
+    -- small blind player name
+    small_blind_seat INT, -- seat number for who was the small blind
+    -- small blind amount
+    -- big blind player name
+    big_blind_seat INT -- seat number for who was the big blind
+    -- big blind amount
 );
 
 -- info on each poker hand
--- removed hand_number, game_type, max_players
+-- added small blind and big blind player name, and the amount of the small and big blind
+ 
