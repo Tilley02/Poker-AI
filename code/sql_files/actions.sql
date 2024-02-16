4592f6d508ef9b4d05aecfa58b9e29dda54a429a
conor tilley <conortilley12@gmail.com>
2024-02-16 13:51:42 +0000

    modified columns in table

--- code/sql_files/actions.sql
+++ code/sql_files/actions.sql
@@ -1,13 +1,12 @@
+-- info on each poker hand
+
 CREATE TABLE actions (
     action_id INT AUTO_INCREMENT PRIMARY KEY,
     hand_id INT,
+    game_phase VARCHAR(50), -- preflop, flop, turn, river, showdown
     player_id INT,
     action_type VARCHAR(50), -- bet, raise, call, fold
     action_amount INT, -- amount of money if any
     FOREIGN KEY (hand_id) REFERENCES Hands(hand_id),
     FOREIGN KEY (player_id) REFERENCES Players(player_id)
-    -- game_phase VARCHAR(50) -- preflop, flop, turn, river, showdown
 );
-
--- info on each poker hand
--- added a game_phase column for the phase of the game
