1f1dbbf45caaa11110508ef18820584610b56a59
conor tilley <conortilley12@gmail.com>
2024-02-17 23:37:48 +0000

    Organised columns better

--- code/sql_files/hands_data.sql
+++ code/sql_files/hands_data.sql
@@ -1,13 +1,13 @@
 CREATE TABLE hands (
     hand_id INT AUTO_INCREMENT PRIMARY KEY,
     table_name VARCHAR(50), -- Pluribus
-    small blind player VARCHAR(50),
+    small_blind_player VARCHAR(50),
     small_blind_seat INT, -- always seat one
-    small blind amount INT,
-    big blind player VARCHAR(50),
-    big_blind_seat INT -- always seat two
-    big blind amount INT
-    button_player INT, -- always seat 6
+    small_blind_amount INT,
+    big_blind_player VARCHAR(50),
+    big_blind_seat INT, -- always seat two
+    big_blind_amount INT,
+    button_player INT -- always seat 6
 );
 
 -- info on each poker hand
