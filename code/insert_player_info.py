05eba05925705a170540c6f277a6d780ef2802f1
conor tilley <conortilley12@gmail.com>
2024-02-18 19:02:53 +0000

    Created files for inserting data into the database
    So can just call them in main file, text_to_sql.py

---
+++ code/insert_player_info.py
@@ -0,0 +1,16 @@
+# Gets player info from hands and inserts into player_data table
+
+import re
+
+def insert_player_info(hand):
+    seat_player_stack_matches = re.findall(r'Seat (\d+): (.+?) \((\d+) in chips\)', hand)
+
+    # uncomment below for loop and the insertion lines to run
+    for seat_num, player_name, starting_stack in seat_player_stack_matches:
+
+        # inserting info into player_data table, only need to do this once per file
+        # insert_game_query = f"INSERT INTO players (player_name, seat_number, chips) VALUES ('{player_name}', {seat_num}, {starting_stack})"
+        # cursor.execute(insert_game_query)
+        # cnx.commit()
+
+        print(f"Seat: {seat_num}, Player: {player_name}, Starting Stack: {starting_stack}") # gets seat num, player, starting chips
