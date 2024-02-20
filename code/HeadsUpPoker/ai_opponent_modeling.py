9430265836dedafed904fb966511151939889979
conor tilley <conortilley12@gmail.com>
2024-02-20 01:13:59 +0000

    Pseudo code for the AI opponent modeling
    Worked on formatting data alsp

--- code/HeadsUpPoker/ai_opponent_modeling.py
+++ code/HeadsUpPoker/ai_opponent_modeling.py
@@ -27,9 +27,11 @@ cursor = cnx.cursor()
 # JOIN hand_summary hs on hc.hand_id = hs.hand_id
 # """
 
+# want to have table formatted like this somewhat
 query = """
-SELECT a.hand_id, a.game_phase, a.player_id, a.action_type, a.action_amount
+SELECT a.hand_id, a.game_phase, a.player_id, a.action_type, a.action_amount, hs.community_cards, hs.pot_size, hs.winner_id, hs.winning_hand
 FROM actions a
+JOIN hand_summary hs ON a.hand_id = hs.hand_id
 """
 
 
@@ -43,11 +45,13 @@ rows = cursor.fetchall()
 input_output_pairs = []
 for row in rows:
     # Process each row and extract relevant information
-    hand_id, game_phase, player_id, action_type, action_amount = row
+    hand_id, game_phase, player_id, action_type, action_amount, community_cards, pot_size, winner_id, winning_hand = row
     # Encode the state of the game and the action taken by the player
     # Append the input-output pair to the list
     # input_output_pairs.append((state_encoding, action_encoding))
-    print(row)
+    # print(row)
+    print(f"Hand ID: {hand_id}, Phase: {game_phase}, Player ID: {player_id}, Action: {action_type}, Amount: {action_amount}, Board Cards: {community_cards}, Pot: {pot_size}, Winner: {winner_id}, Hand: {winning_hand}")
+    print('')
 
 # Convert data to PyTorch tensors
 # features = torch.tensor([pair[0] for pair in input_output_pairs], dtype=torch.float32)
@@ -64,11 +68,15 @@ cnx.close()
 
 
 
-# want to use a simple feedforward neural network
+# Plan for ai model (need to finish formatting data first):
+
+# want to use a simple feedforward neural network (research more)
 
 # input layer: amount of nodes = amount of features from dataset (need to join tables to get the features)
-# hidden layers: 2 hidden layers with 100 nodes each
+# hidden layers: 2 hidden layers with 100 nodes each (should be enough for now)
 # output layer: 1 node for the action taken by the player (to be decided)
-# activation function: relu for hidden layers, sigmoid for output layer
-# loss function: huber loss
-
+# activation function: relu for hidden layers, sigmoid for output layer (used before)
+# loss function: huber loss (can also use MAE, check out both these)
+# optimization algorithm: Adam optimizer
+# hyperparameters: learning rate = 0.001, batch size = 32, epochs = 100, nodes in hidden layers = 100
+# training process: train the model using the input-output pairs, visualize the loss to monitor the training process
