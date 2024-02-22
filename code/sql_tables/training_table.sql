acf7caf2d3822a6408b7bcdd7549e118ff7d3f1c
conor tilley <conortilley12@gmail.com>
2024-02-22 15:42:54 +0000

    Moved result column to the end

--- code/sql_tables/training_table.sql
+++ code/sql_tables/training_table.sql
@@ -18,8 +18,8 @@ CREATE TABLE GameData (
     percentage_of_total_chips_in_pot FLOAT,
     current_stage INT, -- 0 = pre-flop, 1 = flop, 2 = turn, 3 = river
     move INT, -- 0 = fold, 1 = check, 2 = call, 3 = bet, 4 = raise
-    result INT, -- 0 = loss, 1 = win
-    player_hand_ranking INT -- 1 = high card, 2 = pair, 3 = two pair, 4 = three of a kind, 5 = straight, 6 = flush, 7 = full house, 8 = four of a kind, 9 = straight flush, 10 = royal flush
+    player_hand_ranking INT, -- 1 = high card, 2 = pair, 3 = two pair, 4 = three of a kind, 5 = straight, 6 = flush, 7 = full house, 8 = four of a kind, 9 = straight flush, 10 = royal flush
+    result INT -- 0 = loss, 1 = win
 );
 
 -- all info to be converted to numerical values for training
