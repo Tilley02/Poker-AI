7f02f8820841fe3474d1a2fcaebba15f29841268
conor tilley <conortilley12@gmail.com>
2024-02-22 15:43:35 +0000

    AI model work

--- code/HeadsUpPoker/ai_opponent_modeling.py
+++ code/HeadsUpPoker/ai_opponent_modeling.py
@@ -1,85 +1,70 @@
-# file to model the poker ai using pytorch, probably wont need all these imports
+# need to try get hand_strength working, will start ai model setup for now
 
-import mysql.connector
+import sqlalchemy
 import numpy as np
 import pandas as pd # for the visualization
 import seaborn as sns # for the visualization
-import warnings # for the visualization
-import scipy
-import sklearn
 
 # need to check about these
 from sklearn.ensemble import RandomForestClassifier # for the model
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from tqdm.auto import tqdm # used for real time feeback, i.e. progress bar of loops
+from sklearn.metrics import accuracy_score
 
-import sys
-import matplotlib
-import matplotlib.pyplot as plt # for plotting the loss to visualize the training process
-import matplotlib.pyplot as plt # for the visualization
-from itertools import islice # for the visualization
-from types import SimpleNamespace
-from packaging.version import Version
+# check this also
 from scipy.special import huber # for the loss function (can also use MAE)
 
-cnx = mysql.connector.connect(host="localhost",
-                               user="root",
-                               password="12345678",
-                               database="poker_ai_db")
-cursor = cnx.cursor()
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://root:12345678@localhost/poker_ai_db')
 
-# if cnx.is_connected():
+# if engine.connect():
 #     print("Connected to the MySQL database.")
 # else:
 #     print("Not connected to the MySQL database.")
 
+
 query = """
-SELECT gd.S1, gd.C1, gd.S2, gd.C2, gd.S3, gd.C3, gd.S4, gd.C4, gd.S5, gd.C5, gd.S6, gd.C6, gd.S7, gd.C7, 
-       gd.percentage_of_total_chips_hand, gd.percentage_of_hand_bet_pot, gd.percentage_of_total_chips_in_pot,
-       gd.current_stage, gd.move, gd.result, gd.player_hand_ranking
-FROM GameData gd
+SELECT S1, C1, S2, C2, S3, C3, S4, C4, S5, C5, S6, C6, S7, C7, 
+       percentage_of_total_chips_hand, percentage_of_hand_bet_pot, percentage_of_total_chips_in_pot,
+       current_stage, move, result, player_hand_ranking
+FROM GameData
 """
 
-# Execute SQL query and fetch all rows
-cursor = cnx.cursor()
-cursor.execute(query)
-rows = cursor.fetchall()
+# Get information from the table in MySQL
+df = pd.read_sql(query, engine)
+# print(df.head())
+engine.dispose()
 
 
 
 # Left off here thinking about using jupyter for visualization and need to also fix hand_strength code
 
 
+# Get features from the table
+features = df.columns[0:19]
+# print(features)
+X = df[features]
+y = df['result'] 
 
-# Process the data and generate input-output pairs
-input_output_pairs = []
-for row in rows:
-    # Process each row and extract relevant information
-    S1, C1, S2, C2, S3, C3, S4, C4, S5, C5, S6, C6, S7, C7, percentage_of_total_chips_hand, percentage_of_hand_bet_pot, percentage_of_total_chips_in_pot, current_stage, move, result, player_hand_ranking = row
-    # Encode the state of the game and the action taken by the player
-    # Append the input-output pair to the list
-    # input_output_pairs.append((state_encoding, action_encoding))
-    # print(row)
-    # print('S1:', S1, 'C1:', C1, 'S2:', S2, 'C2:', C2, 'S3:', S3, 'C3:', C3, 'S4:', S4, 'C4:', C4, 'S5:', S5, 'C5:', C5, 'S6:', S6, 'C6:', C6, 'S7:', S7, 'C7:', C7, 'percentage_of_total_chips_hand:', percentage_of_total_chips_hand, 'percentage_of_hand_bet_pot:', percentage_of_hand_bet_pot, 'percentage_of_total_chips_in_pot:', percentage_of_total_chips_in_pot, 'current_stage:', current_stage, 'move:', move, 'result:', result, 'player_hand_ranking:', player_hand_ranking)
-    # print('')
-    # print(row)
+# print(X.head())
+# print(y.head())
 
-# sns.scatterplot(data=rows, x="move", y="result")
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=42)
 
+# Initialize and train the model
+model = RandomForestClassifier(n_estimators=10, random_state=42)
+model.fit(X_train, y_train)
 
-# Convert data to PyTorch tensors
-# features = torch.tensor([pair[0] for pair in input_output_pairs], dtype=torch.float32)
-# labels = torch.tensor([pair[1] for pair in input_output_pairs], dtype=torch.long)
+# print('Accuracy on training set: {:.4f}'.format(model.score(X_train, y_train)))
+# print('Accuracy on     test set: {:.4f}'.format(model.score(X_test, y_test)))
 
-# # Verify shapes of tensors
-# print("Features shape:", features.shape)
-# print("Labels shape:", labels.shape)
+# Make predictions on the testing set
+y_pred = model.predict(X_test)
 
 
-# Close the cursor and database connection
-cursor.close()
-cnx.close()
+# Evaluate the model, only works so well so far as dont have hand_strength working
+accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy:", accuracy)
 
 
 
